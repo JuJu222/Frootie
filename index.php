4eb6ca9bc26902844b6450a4e1318319b1653072
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-05-21 11:37:59 +0700

    Added templating for head and navbar, along with css template file

--- index.php
+++ index.php
@@ -2,31 +2,15 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Frootie</title>
+    <?php include "templates/head.php" ?>
     <link rel="stylesheet" href="css/home.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+    <title>Frootie</title>
 </head>
 
 <body>
+<?php include "templates/navbar.php" ?>
 <div class="header">
     <div class="container">
-        <div class="navigationbar">
-            <div class="logo">
-                <img src="img/Frootie_Logo.png">
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Products</a></li>
-                    <li><a href="">Order</a></li>
-                    <li><a href="">Contact Us</a></li>
-                </ul>
-            </nav>
-        </div>
         <div class="row">
             <div class="column">
                 <h1>Try Out Our New <br>Fresh Fruits!</h1>
@@ -40,7 +24,6 @@
         </div>
     </div>
 </div>
-
 
 <!----------- Featured Categories---------------->
 <div class="categories">
