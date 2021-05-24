681e07aa7498981962c4c9aedc4fe52b8dbbb28c
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-05-24 09:48:48 +0700

    Sign in design not working

--- user/sign_in.php
+++ user/sign_in.php
@@ -2,9 +2,40 @@
 <html lang="en">
 <head>
     <?php include "../templates/head.php"?>
+    <link rel="stylesheet" href="../css/signin.css">
     <title>Document</title>
 </head>
 <body>
     <?php include "../templates/navbar.php"?>
+    <div class="registerUI">
+        <div class="logoRegister">
+            <a href="/frootie/index.php"><img src="/frootie/img/Frootie_Logo_Horizontal.png"></a>
+        </div>
+        <div class="registerForm">
+        <form action="" method="POST">
+            <ul>
+                <li>
+                    <h4>Username</h4>
+                    <input type="text" name="username" id="username">
+                </li>
+                <li>
+                    <h4>Password</h4>
+                    <input type="password" name="password" id="password">
+                </li>
+                <li>
+                    <h4>Confirm Password</h4>
+                    <input type="password" name="passwordConfirm" id="passwordConfirm">
+                </li>
+                <li>
+                    <h4>Email</h4>
+                    <input type="text" name="email" id="email">
+                </li>
+                <li>
+                    <button type="submit" name="register">Register</button>
+                </li>
+            </ul>
+        </form>
+        </div>
+    </div>
 </body>
 </html>
--- user/user_controller.php
+++ user/user_controller.php
@@ -9,14 +9,32 @@
 
         if ($password !== $passwordConfirm){
             //Kalau password tidak sesuai
+            echo '<script>
+                    alert ("Check your password again!");
+                  </script>';
             return false;
         }
 
         if (!filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL)){
             //Kalau email salah
+            echo '<script>
+                    alert ("Wrong email format!");
+                  </script>';
             return false;
         }
 
-        
+        $checkResult = mysqli_query($conn, "SELECT username FROM users WHERE username = '$username'");
+        if (mysqli_fetch_assoc($checkResult)){
+            echo "<script>
+                    alert('Username has already been taken!')
+                 </script>";
+            return false;
+        }
+
+        $status = "customer";
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        mysqli_query($conn, "INSERT INTO users VALUES('','$username','$password','$email','$status')");
+        return mysqli_affected_rows($conn);
     }
 ?>
