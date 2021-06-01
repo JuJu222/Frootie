6cb815b7e01e9d2db5ca0eaa27c1cfc0e3a30312
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2021-06-01 19:51:11 +0700

    Update product.php

--- product/product.php
+++ product/product.php
@@ -7,7 +7,7 @@ include "product_controller.php";
 <html lang="en">
 
 <head>
-    <?php include "../templates/head.php"?>
+    <?php include "../templates/head.php" ?>
     <link rel="stylesheet" href="../css/home.css">
     <title>Frootie</title>
 </head>
@@ -18,33 +18,31 @@ include "product_controller.php";
             <?php include "../templates/navbar.php" ?>
         </div>
     </div>
-    <div class="containers">
-        <div class="row2">
-            <?php
-            $perproduk = readProduk();
-            foreach ($perproduk as $key => $item) { ?>
-                <div class="column5">
-                    <div class="picture"></div>
-                    <img src="../img/<?php echo $item['foto_produk']; ?>" alt="">
-                    <h4><?php echo $item['nama_produk']; ?></h4>
-                    <p><?php echo number_format($item['harga_produk']); ?></p>
-                </div>
-                    
-        </div>
-    </div>
+    <div class="categories">
+        <div class="smallContainer">
+            <div class="space"></div>
+            <h2 class="title">Product Catalog</h2>
+            <div class="row">
+                <?php $perproduk = readProduk();
+                foreach ($perproduk as $key => $item) { ?>
+                    <div class="column2">
+                        <img src="../img/<?php echo $item['foto_produk']; ?>" alt="">
+                        <h4><?php echo $item['nama_produk']; ?></h4>
+                        <p><?php echo number_format($item['harga_produk']); ?></p>
+                    </div>
+
+                <?php } ?>
+
+                <!-- <div class="footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="footerColumn1">
+                            </div>
+                            <div class="footerColumn2">
+                            </div>
+                            <div class="footerColumn3">
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
 </body>
-
-    <?php } ?>
-
-<!-- <div class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="footerColumn1">
-            </div>
-            <div class="footerColumn2">
-            </div>
-            <div class="footerColumn3">
-            </div>
-        </div>
-    </div>
-</div> -->
