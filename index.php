65a4e8785c34d78916f0c9d71d4a0375a024cb44
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2021-06-01 20:14:37 +0700

    Footer

--- index.php
+++ index.php
@@ -49,19 +49,39 @@
     </div>
 </div>
 
-
 <div class="footer">
     <div class="container">
         <div class="row">
             <div class="footerColumn1">
+                <h3>Check Out Our Website</h3>
+                <p>We promise you it won't disappoint.</p>
             </div>
             <div class="footerColumn2">
+                <p>Our fruits are 100% authentic, breeded, and harvested from 
+                    the best selection of greenhouses in Indonesia.</p>
             </div>
             <div class="footerColumn3">
+                <h3>What we Offer</h3>
+                <ul>
+                  <li>Freshness</li>
+                  <li>Authenticity</li>
+                  <li>Deliciousness</li>
+                  <li>Expertism</li>
+                </ul>
+            </div>
+            <div class="footerColumn4">
+                <h3>Follow Us</h3>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>Twitter</li>
+                    <li>YouTube</li>
+                </ul>
             </div>
         </div>
     </div>
 </div>
+
 
 </body>
 
